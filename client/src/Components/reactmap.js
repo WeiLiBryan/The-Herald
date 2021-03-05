c1cf294cd3ce2bc175531ae6b5b195ba6900034d
Spencer Christy <spenrad@gmail.com>
2021-03-05 15:49:49 -0800

    commit

--- client/src/Components/reactmap.js
+++ client/src/Components/reactmap.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import ReactMapGL, {Layer, Source} from "react-map-gl";
 import {styleLayer} from "./map-style"
+import API from "../utils/API.js"
 
 function Map() {
   const [viewport, setViewport] = useState({
@@ -9,10 +10,20 @@ function Map() {
     width: "100vw",
     height: "100vh",
     zoom: 2,
-    minZoom: 2
+    minZoom: 2 
   });
 
-  console.log(ReactMapGL)
+  // const [popup, setPopup] = useState("")
+
+  const handleCountrySel = function handleCountrySel (e) {
+    var countryName = e.target
+    console.log(countryName);
+    // API.newsArticles(countryName).then(function (res) {
+    //   console.log(res);
+    // })
+  }; 
+
+  console.log("Map Object", ReactMapGL)
 
   return (
     <ReactMapGL
@@ -21,7 +32,9 @@ function Map() {
       mapStyle= 'mapbox://styles/mapbox/light-v9'
       onViewportChange={viewport => {
           setViewport(viewport);
+          
       }}
+      onClick={handleCountrySel}
     >
         <Source type= 'vector' url= 'mapbox://byfrost-articles.74qv0xp0'>
         <Layer {...styleLayer}/>
