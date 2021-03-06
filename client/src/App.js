e9c9102168f9767b0880bc4dde333d6f07ca512e
Abuye Mamuye <abuyeye3@gmail.com>
2021-03-06 14:39:19 -0800

    navbar setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
-import Map from "./Components/reactmap";
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import { FaAlignRight } from "react-icons/fa";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    toggle: false,
+  };
+  Toggle = () => {
+    this.setState({ toggle: !this.state.toggle });
+  };
+  render() {
+    return (
+      <>
+        <div className="navBar">
+          <button onClick={this.Toggle}>
+            <FaAlignRight />
+          </button>
+          <ul
+            className={this.state.toggle ? "nav-links show-nav" : "nav-links"}
+          >
+            <li href="#">Home</li>
+            <li href="#">About</li>
+            <li href="#">Signup</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
