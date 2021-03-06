bfe95444b88795dc766a589eb4cdf5e57407f10e
Spencer Christy <spenrad@gmail.com>
2021-03-06 11:53:09 -0800

    changed api endpoint to only return articles in english and also the most popular articles at the time

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     newsArticles: function(search) {
-        return axios.get("https://newsapi.org/v2/everything?qInTitle=" +search+ "&sortBy=publishedAt&apiKey=e0f07ff44c4a4914a78bf913d06b2a60")
+        return axios.get("https://newsapi.org/v2/everything?qInTitle=" +search+ "&language=en&sortBy=popularity&apiKey=e0f07ff44c4a4914a78bf913d06b2a60")
     }
 }
