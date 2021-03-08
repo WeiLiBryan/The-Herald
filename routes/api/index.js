ac3a5ac9177c18e3604222feed14d0d0fed9f564
William Bryan <wwbryan00@gmail.com>
2021-03-08 10:27:55 -0800

    add map route to api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const signupRoutes = require("./signup")
-const loginRoutes = require("./login")
+const signupRoutes = require("./signup");
+const loginRoutes = require("./login");
+const mapRoutes = require("./map");
 
 router.use("/signup", signupRoutes)
 router.use("/login", loginRoutes)
+router.use("/map", mapRoutes);
 
 module.exports = router;
