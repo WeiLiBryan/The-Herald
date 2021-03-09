e44dd0e423e88d8746697f0cd7d2e16b610969a2
Abuye Mamuye <abuyeye3@gmail.com>
2021-03-09 11:51:08 -0800

    styleing

--- client/src/Pages/Map/index.js
+++ client/src/Pages/Map/index.js
@@ -1,61 +1,59 @@
 import React, { useState } from "react";
-import "./style.css"
+import "./style.css";
 import Map from "../../Components/reactmap";
 import Popup from "../../Components/Popup";
-import API from "../../utils/API"
+import API from "../../utils/API";
 
 function MapPage() {
-    const [articles, setArticles] = useState([]);
-    const [displayState, setDisplayState] = useState(false);
-    const [currentCountry, setCurrentCountry] = useState("");
+  const [articles, setArticles] = useState([]);
+  const [displayState, setDisplayState] = useState(false);
+  const [currentCountry, setCurrentCountry] = useState("");
 
-    const articleSet = (data) => {
-        setArticles(data);
-    }
+  const articleSet = (data) => {
+    setArticles(data);
+  };
 
-    const changeDisplayState = (bool) => {
-        setDisplayState(bool);
-    }
+  const changeDisplayState = (bool) => {
+    setDisplayState(bool);
+  };
 
-    const Subscribe = function (countryName) {
-        setCurrentCountry(countryName);
-        var countryName = currentCountry; 
-        API.updateTopic(countryName)
-      }
+  const Subscribe = function (countryName) {
+    setCurrentCountry(countryName);
+    var countryName = currentCountry;
+    API.updateTopic(countryName);
+  };
 
-    return (
-        <div>
+  return (
+    <div>
+      <div
+        className="popup"
+        style={{
+          display: displayState ? "block" : "none",
+        }}
+      >
+        <button onClick={Subscribe}>Subrscribe</button>
 
-            <div
-                className="popup"
-                style={{
-                    display: displayState ? "block" : "none",
-                }}
-            >
-                <button onClick={Subscribe}>Subrscribe</button>
-                
-                {articles.splice(0, 5).map((item) => {
-                    return (
-                        <Popup
-                            id={0}
-                            title={item.title}
-                            content={item.content}
-                            description={item.description}
-                            author={item.author}
-                            image={item.urlToImage}
-                        />
-                    );
-                })}
-
-            </div>
-
-            <Map
-                articleSet={articleSet}
-                changeDisplayState={changeDisplayState}
-                Subscribe={Subscribe}
+        {articles.splice(0, 5).map((item) => {
+          return (
+            <Popup
+              id={0}
+              title={item.title}
+              content={item.content}
+              description={item.description}
+              author={item.author}
+              image={item.urlToImage}
             />
-        </div>
-    );
+          );
+        })}
+      </div>
+
+      <Map
+        articleSet={articleSet}
+        changeDisplayState={changeDisplayState}
+        Subscribe={Subscribe}
+      />
+    </div>
+  );
 }
 
-export default MapPage
+export default MapPage;
