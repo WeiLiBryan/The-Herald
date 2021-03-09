6607ef0f8c6e4af46888739a87e84e98c9614f21
William Bryan <wwbryan00@gmail.com>
2021-03-09 13:54:58 -0800

    make dropdown list for profile

--- client/src/Pages/Profile/index.js
+++ client/src/Pages/Profile/index.js
@@ -1,19 +1,61 @@
-import React, { useEffect } from "react";
-import "./style.css"
-import API from "../../utils/API"
+import React, { useState, useEffect } from "react";
+import { Accordion, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import API from "../../utils/API";
 
+function NewsFeed() {
 
+    const [userData, setUserData] = useState();
+    const [articles, setArticles] = useState([]);
 
-function Profile () {
-    
     useEffect(() => {
         API.getUserInfo()
-      }, []);
-    
+            // .then(res => {
+            //     console.log("RES=================",res);
+            //     setUserData(res);
+
+            //     console.log("UserData=================", userData);
+            // })
+    });
+
+    const search = (country) => {
+        API.newsArticles(country)
+            .then(res => {
+                let data = res.data.articles;
+                setArticles(data);
+
+            })
+    }
+
     return (
-        <div>user profile
+        <div>
+            <Accordion>
+
+                {userData.topic.map((country, index) => {
+                    return(
+                        <Card>
+                            <Accordion.Toggle as={Card.Header} eventKey={index} onClick={search(country)}>
+                                {/* Card Title */}
+                                {country}
+                            </Accordion.Toggle>
+
+                            <Accordion.Collapse eventKey={index}>
+                                {/* Card Body */}
+                                {articles.splice(0,5).map(article => {
+                                    <div>
+                                        {article.title}
+                                        {article.author}
+                                    </div>
+                                })};
+                            </Accordion.Collapse>
+                        </Card>
+                    );
+                })};
+
+            </Accordion>
         </div>
-    )
+    );
+
 }
 
-export default Profile
+export default NewsFeed;
