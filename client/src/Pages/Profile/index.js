8b31f2c059843dec27e85b8edba309980f59e2f5
William Bryan <wwbryan00@gmail.com>
2021-03-09 17:57:15 -0800

    generate tabs based on countries in user model

--- client/src/Pages/Profile/index.js
+++ client/src/Pages/Profile/index.js
@@ -2,35 +2,58 @@ import React, { useState, useEffect } from "react";
 import { Accordion, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import API from "../../utils/API";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function NewsFeed() {
-  const [userData, setUserData] = useState([]);
-  const [articles, setArticles] = useState([]);
+    const [userData, setUserData] = useState([]);
+    const [articles, setArticles] = useState([]);
 
-  const getUserData = function () {
-    API.getUserInfo().then(function (res) {
-      console.log("RESPONSE ==========>", res.data.topic);
-      setUserData(res.data.topic);
-    });
-  };
+    const getUserData = function () {
+        API.getUserInfo().then(function (res) {
+            let filtered = res.data.topic.filter(element => element !== null);
+            console.log("RESPONSE ==========>", filtered);
+            setUserData(filtered);
+        });
+    };
 
-  useEffect(function () {
-    getUserData();
-  }, []);
+    useEffect(function () {
+        getUserData();
+    }, []);
 
-  const search = (country) => {
-    API.newsArticles(country).then((res) => {
-      let data = res.data.articles;
-      setArticles(data);
-    });
-  };
+    const search = (country) => {
+        API.newsArticles(country).then((res) => {
+            let data = res.data.articles;
+            
+            setArticles(data);
+        });
+    };
 
-  return (
-    <div>
-        
-    </div>
-    
-  );
+    return (
+        <Tabs>
+            <TabList>
+                {/* MAPS THE SAVED COUNTRIES FOR TAB HEADERS*/}
+                {userData.map(country => {
+                    console.log(country);
+                    
+                    return (
+                        <Tab onClick={() => search()}>{country}</Tab>
+                    );
+                })}
+                {/* <Tab>Hello</Tab> */}
+            </TabList>
+
+                {/* SEARCHES API WITH COUNTRY NAME AND OUTPUTS THE ARTICLES HERE */}
+            {articles.splice(0, 5).map(article => {
+                <TabPanel>
+                    <h5>{article.title}</h5>
+                    <h6>{article.author}</h6>
+                    <p>{article.description}</p>
+                </TabPanel>
+            })}
+
+        </Tabs>
+    );
 }
 
 export default NewsFeed;
