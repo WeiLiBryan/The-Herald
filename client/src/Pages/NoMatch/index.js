01387cafa7109021924cd542ba576da5503f0e1a
Spencer Christy <spenrad@gmail.com>
2021-03-10 16:21:19 -0800

    added bg video to noauth and nomatch pages

--- client/src/Pages/NoMatch/index.js
+++ client/src/Pages/NoMatch/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import bg from "../../Components/bgVideo.mp4";
 
 const NoMatch = () => {
   return (
-    <div className="container-fluid">
+    <div>
+    {/* Background video */}
+    <video autoPlay muted loop id="bgVideo">
+    <source src={bg} type="video/mp4" />
+   </video>
+
+    <div className="content">
       <h1>
         4<span role="img" aria-label="Globe Emoji">🌎</span>4 Page Not Found
       </h1>
+    </div>
     </div>
   );
 };
--- client/src/Pages/NotAuth/index.js
+++ client/src/Pages/NotAuth/index.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import bg from "../../Components/bgVideo.mp4";
 
 
 const NotAuth = () => {
   return (
-    <div className="container-fluid">
+    <div>
+     {/* Background video */}
+     <video autoPlay muted loop id="bgVideo">
+     <source src={bg} type="video/mp4" />
+    </video>
+    
+    <div className="content">
       <div className="row">
         <div className="col-md-12">
-          <div className="hero">
             <h1>You are not authorized to visit this page!</h1>
             <div><Link to="/">Please Make an Account</Link>,  <Link to="/login">Or Sign-in</Link></div>
-          </div>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
