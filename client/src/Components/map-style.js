b1405d9381edd173e32bbad184788d86d013364b
William Bryan <wwbryan00@gmail.com>
2021-03-10 21:21:43 -0800

    redesigned popup

--- client/src/Components/map-style.js
+++ client/src/Components/map-style.js
@@ -3,7 +3,7 @@ export const styleLayer = {
     'source-layer': 'ne_10m_admin_0_countries-76t9ly',
     type: 'fill',
     paint: {
-      'fill-color': 'rgba(82, 72, 156, .12)',
+      'fill-color': 'rgba(16,52,166,.3)',
       'fill-outline-color': 'rgba(82, 72, 156, 1)'
     }
 }
